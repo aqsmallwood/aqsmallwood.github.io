818edc30d1d46d3fe2648e5b6355d4a535c1f403
Adrian Smallwood <smallwoodadrian54@gmail.com>
2025-02-28 23:01:28 -0800

    path

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   site: 'https://your-dog-breeding-company.com',
   integrations: [tailwind()],
+  base: '/amc360',
   // Add any other configurations as needed
 }); 
